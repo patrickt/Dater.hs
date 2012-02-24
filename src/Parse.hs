b36bfdd8cb2f0fb47b1f14ffc11781a3a176b371
Patrick Thomson <pthomson@apple.com>
2012-02-23 22:11:00 -0500

    Get rid of the unused stuff.

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Parse
     ( Parser
     , Parse(parse)
@@ -9,32 +9,25 @@ module Parse
     , dot
     , slash
     , whitespace
-    , whited
-    , liftReadS
     ) where
 
 import Control.Applicative
 import Control.Monad
 import Data.Maybe (listToMaybe)
 import Data.Ratio ((%))
-import Text.ParserCombinators.Parsec hiding (Parser, parse, many)
+import Text.ParserCombinators.Parsec hiding (Parser, parse, many, optional)
 
 type Parser = GenParser Char ()
+
 class Parse a where parse :: Parser a
+
 instance Parse Int where parse = read <$> many1 digit
 instance Parse Integer where parse = read <$> many1 digit
 instance Parse Rational where parse = (%) <$> (parse <* slash) <*> parse
-instance Parse a => Parse (Maybe a) where parse = optionMaybe parse
+instance Parse a => Parse (Maybe a) where parse = optional parse
 
 colon, dot, slash, whitespace :: Parser ()
 colon      = void $ char ':'
 dot        = void $ char '.'
 slash      = void $ char '\\'
 whitespace = void $ oneOf " \t"
-
-whited :: Parser a -> Parser a
-whited x = w *> x <* w where w = many whitespace
-
-liftReadS :: ReadS a -> String -> Parser a
-liftReadS f = maybe (unexpected "no parse") (pure . fst) .
-              listToMaybe . filter (null . snd) . f
